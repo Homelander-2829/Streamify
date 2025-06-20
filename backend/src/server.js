142bf4d956cd51680e172d958cf0979ab06a18a5
Homelander-2829 <yashpardeshi0755@gmail.com>
2025-06-20 19:29:09 +0530

    Fix: use process.env.PORT for Vercel compatibility

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,13 +37,8 @@ app.use("/api/chat", chatRoutes);
 //   });
 // }
 
-await connectDB();
-
-if(process.env.NODE_ENV !== "production") {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-      });      
-}
+        connectDB();
+    });      
 
-// export server for vercel
-export default app;
